e51227181adfceebdadcef833a56da6d4294cb2e
gendo37 <39008537+gendo37@users.noreply.github.com>
2018-12-03 00:15:58 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,9 +17,9 @@ def load_opus_lib(opus_libs=OPUS_LIBS):
     for opus_lib in opus_libs:
 	    try:
 	        opus.load_opus(opus_lib)
-	    return
-	    except OSError:
-		pass
+	       return
+            except OSError:
+	       pass
 	
     raise RuntimeError('Could not load an opus lib. Tried %s' % 
 		       (', '.join(opus_libs)))
